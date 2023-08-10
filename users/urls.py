ae66a3486ef5e1185e2431406af0da632fd95a29
My name <me@example.com>
2023-08-10 15:53:12 +0530

    user registeration and login

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
 
+from .views import getRoutes,RetrieveUserView,RegisterUser
+
 urlpatterns = [
+    path('',getRoutes,name='getRoutes'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('users/me/',RetrieveUserView.as_view() ,name='getRoutes'),
+    path('register/',RegisterUser.as_view(),name='register'),
+    
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view
+
+from rest_framework.views import APIView
+from rest_framework import permissions,status
+
+from .serializer import UserSerializer,UserCreateSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['is_superuser'] = user.is_superuser
+        token['email'] = user.email
+        # ...
+
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        'api/token/',
+        'api/token/refresh/',
+        'api/token/verify/',
+        'users/me/',
+        'register/'
+    ]
+    
+    return Response(routes)
+
+
+class RegisterUser(APIView):
+    def post(self,request):
+        serializer = UserCreateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        user = serializer.create(serializer.validated_data)
+        user = UserSerializer(user)
+        return Response(user.data,status=status.HTTP_201_CREATED)
+    
+class RetrieveUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        user = UserSerializer(user)
+        return Response(user.data, status=status.HTTP_200_OK)
+        
