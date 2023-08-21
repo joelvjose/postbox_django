b542e0ec7a12c28ebde2f955407e25cda8f59ecb
My name <me@example.com>
2023-08-21 21:05:59 +0530

    serializer updated for like and reported posts

--- post/serializer.py
+++ post/serializer.py
@@ -14,7 +14,15 @@ class UserSerializer(serializers.ModelSerializer):
 
 class  PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only = True)
+    likes_count = serializers.SerializerMethodField()
+    reports_count = serializers.SerializerMethodField()
+    
+    def get_likes_count(self, obj):
+        return obj.total_likes()
+    
+    def get_reports_count(self, obj):
+        return obj.total_reports()
     
     class Meta:
         model = posts
-        fields = ['body','img','author','created_time','id']
+        fields = ['id','body','img','author','created_time','likes', 'likes_count','reports_count']
